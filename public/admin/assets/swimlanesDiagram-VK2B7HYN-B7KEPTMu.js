d378c4b14329e983c120d5567d144218aa110e95
Carlos Ozuna <cozuna@Carloss-MacBook-Air.local>
2026-08-23 18:46:10 -0400

    refactor: Migrate back to Sveltia CMS and configure Block Builder

---
+++ public/admin/assets/swimlanesDiagram-VK2B7HYN-B7KEPTMu.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-A5DVABFB-z5q4hzNn.js";import{_ as a}from"./index-D_j169dl.js";import"./chunk-5VM5RSS4-CPDw2BAu.js";import"./chunk-XXDRQBXY-Bhz1iDfr.js";import"./chunk-POPQ4Y6H-BbaxiICS.js";import"./chunk-F27PBJKO-BdJXslQi.js";import"./channel-B802jEu-.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
--- src/content/custom-pages/test.md
+++ src/content/custom-pages/test.md
@@ -2,14 +2,13 @@
 title: Test Page
 meta_description: This is a test
 blocks:
-  - headline: Hello World
+  - type: hero
+    headline: Hello World
     subheadline: This is a block builder
     button:
       text: Click Me
       link: /contact
-    _template: hero
-  - content: >-
+  - type: text
+    content: >-
       This is some **markdown** text!
-    _template: text
 ---
-test body
