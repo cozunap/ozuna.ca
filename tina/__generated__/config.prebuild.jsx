a084381b3b8d3ab30d115035cd32d8bf93f68ce3
Carlos Ozuna <cozuna@Carloss-MacBook-Air.local>
2026-08-23 17:39:12 -0400

    Migrate to TinaCMS for visual editing

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,293 @@
+// tina/config.ts
+import { defineConfig } from "tinacms";
+var branch = process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD || "main";
+var config_default = defineConfig({
+  branch,
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "dummy-client-id",
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN || "dummy-token",
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "assets/uploads",
+      publicFolder: "public"
+    }
+  },
+  schema: {
+    collections: [
+      {
+        name: "work",
+        label: "Work",
+        path: "src/content/work",
+        ui: {
+          router: ({ document }) => {
+            return `/${document._sys.filename}`;
+          }
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "string",
+            name: "category",
+            label: "Category",
+            options: ["web", "graphic"]
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Image"
+          },
+          {
+            type: "string",
+            name: "link",
+            label: "Website Link"
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            ui: {
+              component: "textarea"
+            }
+          },
+          {
+            type: "image",
+            name: "gallery",
+            label: "Gallery",
+            list: true
+          },
+          {
+            type: "string",
+            name: "meta_description",
+            label: "Meta Description"
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          }
+        ]
+      },
+      {
+        name: "pages",
+        label: "Pages",
+        path: "src/content/pages",
+        match: {
+          include: "home"
+        },
+        ui: {
+          allowedActions: {
+            create: false,
+            delete: false
+          },
+          router: () => "/"
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "string",
+            name: "meta_description",
+            label: "Meta Description"
+          },
+          {
+            type: "string",
+            name: "heroTitle",
+            label: "Hero Title HTML"
+          },
+          {
+            type: "image",
+            name: "backgroundVideo",
+            label: "Background Video"
+          },
+          {
+            type: "object",
+            name: "buttons",
+            label: "Buttons",
+            list: true,
+            fields: [
+              {
+                type: "string",
+                name: "text",
+                label: "Button Text"
+              },
+              {
+                type: "string",
+                name: "link",
+                label: "Link/File"
+              },
+              {
+                type: "boolean",
+                name: "isExternal",
+                label: "Open in new tab?"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "about",
+        label: "About Me Page",
+        path: "src/content/pages",
+        match: {
+          include: "about"
+        },
+        ui: {
+          allowedActions: {
+            create: false,
+            delete: false
+          },
+          router: () => "/about-me"
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "string",
+            name: "meta_description",
+            label: "Meta Description"
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          },
+          {
+            type: "object",
+            name: "contactLinks",
+            label: "Contact Links",
+            list: true,
+            fields: [
+              {
+                type: "string",
+                name: "text",
+                label: "Link Text"
+              },
+              {
+                type: "string",
+                name: "link",
+                label: "URL/File"
+              },
+              {
+                type: "boolean",
+                name: "isExternal",
+                label: "Open in new tab?"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: "work_page",
+        label: "Work Page",
+        path: "src/content/pages",
+        match: {
+          include: "work"
+        },
+        ui: {
+          allowedActions: {
+            create: false,
+            delete: false
+          },
+          router: () => "/work"
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "string",
+            name: "meta_description",
+            label: "Meta Description"
+          },
+          {
+            type: "string",
+            name: "subtitle",
+            label: "Subtitle"
+          }
+        ]
+      },
+      {
+        name: "contact",
+        label: "Contact Page",
+        path: "src/content/pages",
+        match: {
+          include: "contact"
+        },
+        ui: {
+          allowedActions: {
+            create: false,
+            delete: false
+          },
+          router: () => "/contact"
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "string",
+            name: "meta_description",
+            label: "Meta Description"
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          },
+          {
+            type: "object",
+            name: "actionButton",
+            label: "Action Button",
+            fields: [
+              {
+                type: "string",
+                name: "text",
+                label: "Button Text"
+              },
+              {
+                type: "string",
+                name: "link",
+                label: "Email Address / Link"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
---
+++ tina/__generated__/types.js
@@ -0,0 +1,418 @@
+export function gql(strings, ...args) {
+  let str = "";
+  strings.forEach((string, i) => {
+    str += string + (args[i] || "");
+  });
+  return str;
+}
+export const WorkPartsFragmentDoc = gql`
+    fragment WorkParts on Work {
+  __typename
+  title
+  category
+  image
+  link
+  description
+  gallery
+  meta_description
+  body
+}
+    `;
+export const PagesPartsFragmentDoc = gql`
+    fragment PagesParts on Pages {
+  __typename
+  title
+  meta_description
+  heroTitle
+  backgroundVideo
+  buttons {
+    __typename
+    text
+    link
+    isExternal
+  }
+}
+    `;
+export const AboutPartsFragmentDoc = gql`
+    fragment AboutParts on About {
+  __typename
+  title
+  meta_description
+  body
+  contactLinks {
+    __typename
+    text
+    link
+    isExternal
+  }
+}
+    `;
+export const Work_PagePartsFragmentDoc = gql`
+    fragment Work_pageParts on Work_page {
+  __typename
+  title
+  meta_description
+  subtitle
+}
+    `;
+export const ContactPartsFragmentDoc = gql`
+    fragment ContactParts on Contact {
+  __typename
+  title
+  meta_description
+  body
+  actionButton {
+    __typename
+    text
+    link
+  }
+}
+    `;
+export const WorkDocument = gql`
+    query work($relativePath: String!) {
+  work(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        hasReferences
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...WorkParts
+  }
+}
+    ${WorkPartsFragmentDoc}`;
+export const WorkConnectionDocument = gql`
+    query workConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: WorkFilter) {
+  workConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            hasReferences
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...WorkParts
+      }
+    }
+  }
+}
+    ${WorkPartsFragmentDoc}`;
+export const PagesDocument = gql`
+    query pages($relativePath: String!) {
+  pages(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        hasReferences
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...PagesParts
+  }
+}
+    ${PagesPartsFragmentDoc}`;
+export const PagesConnectionDocument = gql`
+    query pagesConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: PagesFilter) {
+  pagesConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            hasReferences
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...PagesParts
+      }
+    }
+  }
+}
+    ${PagesPartsFragmentDoc}`;
+export const AboutDocument = gql`
+    query about($relativePath: String!) {
+  about(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        hasReferences
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...AboutParts
+  }
+}
+    ${AboutPartsFragmentDoc}`;
+export const AboutConnectionDocument = gql`
+    query aboutConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: AboutFilter) {
+  aboutConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            hasReferences
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...AboutParts
+      }
+    }
+  }
+}
+    ${AboutPartsFragmentDoc}`;
+export const Work_PageDocument = gql`
+    query work_page($relativePath: String!) {
+  work_page(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        hasReferences
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...Work_pageParts
+  }
+}
+    ${Work_PagePartsFragmentDoc}`;
+export const Work_PageConnectionDocument = gql`
+    query work_pageConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: Work_pageFilter) {
+  work_pageConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            hasReferences
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...Work_pageParts
+      }
+    }
+  }
+}
+    ${Work_PagePartsFragmentDoc}`;
+export const ContactDocument = gql`
+    query contact($relativePath: String!) {
+  contact(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        hasReferences
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...ContactParts
+  }
+}
+    ${ContactPartsFragmentDoc}`;
+export const ContactConnectionDocument = gql`
+    query contactConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: ContactFilter) {
+  contactConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            hasReferences
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...ContactParts
+      }
+    }
+  }
+}
+    ${ContactPartsFragmentDoc}`;
+export function getSdk(requester) {
+  return {
+    work(variables, options) {
+      return requester(WorkDocument, variables, options);
+    },
+    workConnection(variables, options) {
+      return requester(WorkConnectionDocument, variables, options);
+    },
+    pages(variables, options) {
+      return requester(PagesDocument, variables, options);
+    },
+    pagesConnection(variables, options) {
+      return requester(PagesConnectionDocument, variables, options);
+    },
+    about(variables, options) {
+      return requester(AboutDocument, variables, options);
+    },
+    aboutConnection(variables, options) {
+      return requester(AboutConnectionDocument, variables, options);
+    },
+    work_page(variables, options) {
+      return requester(Work_PageDocument, variables, options);
+    },
+    work_pageConnection(variables, options) {
+      return requester(Work_PageConnectionDocument, variables, options);
+    },
+    contact(variables, options) {
+      return requester(ContactDocument, variables, options);
+    },
+    contactConnection(variables, options) {
+      return requester(ContactConnectionDocument, variables, options);
+    }
+  };
+}
+import { createClient } from "tinacms/dist/client";
+const generateRequester = (client) => {
+  const requester = async (doc, vars, options) => {
+    let url = client.apiUrl;
+    if (options?.branch) {
+      const index = client.apiUrl.lastIndexOf("/");
+      url = client.apiUrl.substring(0, index + 1) + options.branch;
+    }
+    const data = await client.request({
+      query: doc,
+      variables: vars,
+      url
+    }, options);
+    return { data: data?.data, errors: data?.errors, query: doc, variables: vars || {} };
+  };
+  return requester;
+};
+export const ExperimentalGetTinaClient = () => getSdk(
+  generateRequester(
+    createClient({
+      url: "http://localhost:4001/graphql",
+      queries
+    })
+  )
+);
+export const queries = (client) => {
+  const requester = generateRequester(client);
+  return getSdk(requester);
+};
